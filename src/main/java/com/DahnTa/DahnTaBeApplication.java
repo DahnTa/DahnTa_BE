8c93e8990c762fe82755a37c4666f2fae10d996f
unknown <kim0615157@gmail.com>
2025-11-22 18:58:48 +0900

    feat:init

---
+++ src/main/java/com/DahnTa/DahnTaBeApplication.java
@@ -0,0 +1,13 @@
+package com.DahnTa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DahnTaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DahnTaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DahnTa/DahnTaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DahnTa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DahnTaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
