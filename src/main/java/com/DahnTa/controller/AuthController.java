fcddf7d16caed10c40c8520c3606f4edc02f828a
peacefullyquietly <pjh73015@gmail.com>
2025-12-04 23:44:12 +0900

    feat : AuthController 내 /password 에 대한 메서드 생성

--- src/main/java/com/DahnTa/controller/AuthController.java
+++ src/main/java/com/DahnTa/controller/AuthController.java
@@ -2,13 +2,16 @@ package com.DahnTa.controller;
 
 import com.DahnTa.dto.Auth.LoginRequestDTO;
 import com.DahnTa.dto.Auth.LoginResponseDTO;
+import com.DahnTa.dto.Auth.PasswordRequestDTO;
 import com.DahnTa.dto.Auth.SignUpRequestDTO;
 import com.DahnTa.service.AuthService;
+import com.DahnTa.service.JWTService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+    private final JWTService jwtService;
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody SignUpRequestDTO signUpRequestDTO) {
@@ -31,10 +35,24 @@ public class AuthController {
     }
 
     @PostMapping("/password")
-    public static void editPassword() {
+    public ResponseEntity<?> updatePassword(@RequestHeader("Authorization") String token,
+       @RequestBody PasswordRequestDTO updatePassword) throws Exception {
+        String actualToken = token;
+        if (token.startsWith("Bearer ")) {
+            actualToken = token.substring(7);
+        }
+
+        // 토큰 검증
+        if (jwtService.validateToken(actualToken)) {
+            // @todo : token pasring으로 Id 추출
+             Long userId = jwtService.extractUserId(actualToken);
+
+            authService.changePassword(userId, updatePassword.getPassword());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+          throw new Exception("토큰이 유효하지 않음");
+        }
 
     }
-
-
 
 }
