e608d1ddd4f8cbae48f7fb2d23385654c11d37f8
peacefullyquietly <pjh73015@gmail.com>
2025-12-04 23:44:50 +0900

    feat : PasswordRequestDTO 클래스 생성

---
+++ src/main/java/com/DahnTa/dto/Auth/PasswordRequestDTO.java
@@ -0,0 +1,13 @@
+package com.DahnTa.dto.Auth;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PasswordRequestDTO {
+    private String password;
+
+}
