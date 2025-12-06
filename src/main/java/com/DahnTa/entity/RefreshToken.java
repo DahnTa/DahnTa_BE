f882ba47b1c11332771884809d9848251545fb04
peacefullyquietly <pjh73015@gmail.com>
2025-12-06 10:22:42 +0900

    refactor : Lombok 어노테이션 적용

--- src/main/java/com/DahnTa/entity/RefreshToken.java
+++ src/main/java/com/DahnTa/entity/RefreshToken.java
@@ -10,11 +10,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "REFRESH_TOKEN_TB")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class RefreshToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,26 +26,14 @@ public class RefreshToken {
     @JoinColumn(name = "user_id", unique = true)
     private User user;
 
-    @Column(name = "refreshToken", nullable = false)
+
+    // @todo : snake case
+    @Column(name = "refresh_token", nullable = false)
     private String refreshToken;
 
     public RefreshToken(User user, String refreshToken) {
         this.user = user;
         this.refreshToken = refreshToken;
-    }
-
-    //@todo : getter 어노테이션
-    public User getUser() {
-        return user;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-
-    public String getRefreshToken() {
-        return refreshToken;
     }
 
 
