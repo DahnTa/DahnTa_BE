d1327befbcfce1fbdb4cfb775455c4c7faf1a34d
peacefullyquietly <pjh73015@gmail.com>
2025-12-06 10:19:44 +0900

    feat : UserStockController 클래스 생성

---
+++ src/main/java/com/DahnTa/controller/UserStockController.java
@@ -0,0 +1,38 @@
+package com.DahnTa.controller;
+
+import com.DahnTa.service.JWTService;
+import com.DahnTa.service.UserStockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserStockController {
+    private final UserStockService userStockService;
+
+    @PostMapping("/{id}/dislike")
+    public ResponseEntity<?> dislikePost(@RequestHeader("Authorization") String token,
+        @PathVariable Long id) throws Exception {
+
+        userStockService.applyDislike(id, token);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    @PostMapping("/{id}/like")
+    public ResponseEntity<?> likePost(@RequestHeader("Authorization") String token,
+        @PathVariable Long id) {
+
+        userStockService.applyLike(id, token);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
