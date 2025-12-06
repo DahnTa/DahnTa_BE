3f8feccc2fd53bf5c529af213ec265539dc343ba
peacefullyquietly <pjh73015@gmail.com>
2025-12-06 10:25:18 +0900

    feat : InterestRepository 클래스 생성

---
+++ src/main/java/com/DahnTa/repository/InterestRepository.java
@@ -0,0 +1,12 @@
+package com.DahnTa.repository;
+
+import com.DahnTa.entity.Interest;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InterestRepository extends JpaRepository<Interest, Long> {
+    Optional<Interest> findByUserIdAndStockId(Long userId, Long stockId);
+    boolean existsByUserIdAndStockId(Long userId, Long stockId);
+}
