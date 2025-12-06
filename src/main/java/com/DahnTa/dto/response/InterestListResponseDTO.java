e6d0e77d501b5d35a9645cbfbae81e01992f75b2
peacefullyquietly <pjh73015@gmail.com>
2025-12-06 14:23:09 +0900

    chore : 사용하지 않는 코드 제거

--- src/main/java/com/DahnTa/dto/response/InterestListResponseDTO.java
+++ src/main/java/com/DahnTa/dto/response/InterestListResponseDTO.java
@@ -2,7 +2,6 @@ package com.DahnTa.dto.response;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 public class InterestListResponseDTO {
