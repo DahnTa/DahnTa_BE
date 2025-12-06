723184bede99fd217f8e98f60e36befa0ae9a7bf
peacefullyquietly <pjh73015@gmail.com>
2025-12-06 13:12:19 +0900

    feat : Interest, InterestList Response DTO 클래스 생성

---
+++ src/main/java/com/DahnTa/dto/response/InterestListResponseDTO.java
@@ -0,0 +1,11 @@
+package com.DahnTa.dto.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+public class InterestListResponseDTO {
+    List<InterestResponseDTO> interestResponseDTOList;
+
+}
