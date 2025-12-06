79babd5a9c74b2fa99a218df1be1144c3bcfe8c2
peacefullyquietly <pjh73015@gmail.com>
2025-12-06 14:02:28 +0900

    feat : AssetResponseDTO 클래스 생성

---
+++ src/main/java/com/DahnTa/dto/response/AssetResponseDTO.java
@@ -0,0 +1,13 @@
+package com.DahnTa.dto.response;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class AssetResponseDTO {
+    private double totalAmount;
+    private double creditChangeRate;
+    private double creditChangeAmount;
+    private double userCredit;
+    private double stockValuation;
+
+}
