7ac2a97adfe2894ca22803e69b5e8900ef463133
unknown <kim0615157@gmail.com>
2025-12-30 14:53:33 +0900

    feat:dahnTa-base-data

---
+++ data/insert_stock.sql
@@ -0,0 +1,12 @@
+-- stock_tb 테이블 INSERT 스크립트
+
+INSERT INTO stock_tb (id, stock_name, stock_tag) VALUES (1, '엔비디아', 'NVDA');
+INSERT INTO stock_tb (id, stock_name, stock_tag) VALUES (2, '애플', 'AAPL');
+INSERT INTO stock_tb (id, stock_name, stock_tag) VALUES (3, '마이크로소프트', 'MSFT');
+INSERT INTO stock_tb (id, stock_name, stock_tag) VALUES (4, '알파벳', 'GOOGL');
+INSERT INTO stock_tb (id, stock_name, stock_tag) VALUES (5, '아마존', 'AMZN');
+INSERT INTO stock_tb (id, stock_name, stock_tag) VALUES (6, '메타', 'META');
+INSERT INTO stock_tb (id, stock_name, stock_tag) VALUES (7, '브로드컴', 'AVGO');
+INSERT INTO stock_tb (id, stock_name, stock_tag) VALUES (8, '테슬라', 'TSLA');
+INSERT INTO stock_tb (id, stock_name, stock_tag) VALUES (9, '버크셔 헤서웨이', 'BRK.B');
+INSERT INTO stock_tb (id, stock_name, stock_tag) VALUES (10, '월마트', 'WMT');
